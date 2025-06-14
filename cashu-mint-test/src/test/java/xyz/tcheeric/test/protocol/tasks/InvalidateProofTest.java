5027efde03430c1d94bd8e02edae9598531f0109
erict875 <erict875@tuta.io>
2025-06-14 01:03:17 +0100

    Refactor import statements in InvalidateProofTest for improved organization and clarity

--- cashu-mint-test/src/test/java/xyz/tcheeric/test/protocol/tasks/InvalidateProofTest.java
+++ cashu-mint-test/src/test/java/xyz/tcheeric/test/protocol/tasks/InvalidateProofTest.java
@@ -3,17 +3,17 @@ package xyz.tcheeric.test.protocol.tasks;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import xyz.tcheeric.cashu.common.model.BlindedMessage;
-import xyz.tcheeric.cashu.common.model.Mint;
-import xyz.tcheeric.cashu.common.model.PublicKey;
-import xyz.tcheeric.cashu.common.model.RSSProof;
-import xyz.tcheeric.cashu.common.model.RandomStringSecret;
-import xyz.tcheeric.cashu.common.model.Signature;
-import xyz.tcheeric.cashu.common.model.rest.PostSwapRequest;
+import xyz.tcheeric.cashu.common.BlindedMessage;
+import xyz.tcheeric.cashu.common.Mint;
+import xyz.tcheeric.cashu.common.PublicKey;
+import xyz.tcheeric.cashu.common.RSSProof;
+import xyz.tcheeric.cashu.common.RandomStringSecret;
+import xyz.tcheeric.cashu.common.Signature;
 import xyz.tcheeric.cashu.common.util.CashuErrorException;
 import xyz.tcheeric.cashu.crypto.BDHKEUtils;
+import xyz.tcheeric.cashu.crypto.util.Utils;
+import xyz.tcheeric.cashu.entities.rest.PostSwapRequest;
 import xyz.tcheeric.cashu.mint.proto.tasks.InvalidateProofsTask;
-import cashu.util.Utils;
 import xyz.tcheeric.cashu.vault.config.MintConfiguration;
 import xyz.tcheeric.cashu.vault.config.ProofConfiguration;
 import xyz.tcheeric.cashu.vault.impl.fs.FSMintVault;
@@ -56,7 +56,7 @@ public class InvalidateProofTest {
 
     @Test
     public void invalidateProof() throws CashuErrorException {
-        PostSwapRequest request = new PostSwapRequest();
+        PostSwapRequest<RandomStringSecret> request = new PostSwapRequest();
 
         RSSProof proof = new RSSProof();
         proof.setUnblindedSignature(Signature.fromString("0392810a73efd77346d3658bf0dc7004fae1e201a03bd511d8077956d7785a8355"));
