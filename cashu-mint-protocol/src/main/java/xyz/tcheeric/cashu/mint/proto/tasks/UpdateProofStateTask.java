9e51d5dc5c41a2aab4ce7d83127c01b8c165a2ff
erict875 <erict875@tuta.io>
2025-06-14 01:12:15 +0100

    Refactor UpdateProofStateTask class for type safety
    Introduced generics to the `UpdateProofStateTask` class by parameterizing `Proof` with `<T extends Secret>`. This improves type safety and aligns with modern Java best practices across the codebase. Updated imports to reflect this change and removed unused ones.

--- cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/tasks/UpdateProofStateTask.java
+++ cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/tasks/UpdateProofStateTask.java
@@ -1,20 +1,21 @@
 package xyz.tcheeric.cashu.mint.proto.tasks;
 
-import cashu.util.Utils;
-import xyz.tcheeric.cashu.common.model.Mint;
-import xyz.tcheeric.cashu.common.model.Proof;
+import lombok.AllArgsConstructor;
+import xyz.tcheeric.cashu.common.Mint;
+import xyz.tcheeric.cashu.common.Proof;
+import xyz.tcheeric.cashu.common.Secret;
 import xyz.tcheeric.cashu.common.util.CashuErrorException;
 import xyz.tcheeric.cashu.common.util.Task;
 import xyz.tcheeric.cashu.crypto.BDHKEUtils;
+import xyz.tcheeric.cashu.crypto.util.Utils;
 import xyz.tcheeric.cashu.vault.config.MintConfiguration;
 import xyz.tcheeric.cashu.vault.config.ProofConfiguration;
 import xyz.tcheeric.cashu.vault.impl.fs.FSProofVault;
-import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class UpdateProofStateTask implements Task<Boolean> {
+public class UpdateProofStateTask<T extends Secret> implements Task<Boolean> {
     private final Mint mint;
-    private final Proof proof;
+    private final Proof<T> proof;
 
     public Boolean execute() throws CashuErrorException {
         String unblindedSignature = proof.getUnblindedSignature().toString();
