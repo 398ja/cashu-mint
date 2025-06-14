b1d2ed6c746f81702b78136e06b07ccaffa22a69
erict875 <erict875@tuta.io>
2025-06-14 01:02:17 +0100

    Refactor import statements in FSKeyVault for improved organization and clarity

--- cashu-mint-vault/src/main/java/xyz/tcheeric/cashu/vault/impl/fs/FSKeyVault.java
+++ cashu-mint-vault/src/main/java/xyz/tcheeric/cashu/vault/impl/fs/FSKeyVault.java
@@ -1,15 +1,15 @@
 package xyz.tcheeric.cashu.vault.impl.fs;
 
-import xyz.tcheeric.cashu.common.model.Keys;
-import xyz.tcheeric.cashu.common.model.PrivateKey;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.extern.java.Log;
+import xyz.tcheeric.cashu.common.Keys;
+import xyz.tcheeric.cashu.common.PrivateKey;
 import xyz.tcheeric.cashu.common.util.CashuErrorException;
 import xyz.tcheeric.cashu.vault.FSVault;
 import xyz.tcheeric.cashu.vault.config.KeyConfiguration;
 import xyz.tcheeric.cashu.vault.config.KeysetConfiguration;
 import xyz.tcheeric.cashu.vault.config.MintConfiguration;
-import lombok.AllArgsConstructor;
-import lombok.NonNull;
-import lombok.extern.java.Log;
 
 import java.io.IOException;
 import java.math.BigInteger;
