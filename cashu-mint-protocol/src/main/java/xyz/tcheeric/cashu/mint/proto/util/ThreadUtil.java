e7e73b82b37f2dec160850418543acddc7e7fecf
erict875 <erict875@tuta.io>
2025-06-14 01:12:05 +0100

    Remove deprecated annotation and unused lock in ThreadUtil
    Eliminated the `@Deprecated` annotation and the unused `PROOF_STATE_LOCK` field from the `ThreadUtil` class. This cleanup reduces unnecessary code and enhances maintainability.

--- cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/util/ThreadUtil.java
+++ cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/util/ThreadUtil.java
@@ -2,8 +2,6 @@ package xyz.tcheeric.cashu.mint.proto.util;
 
 import java.util.concurrent.locks.ReentrantLock;
 
-@Deprecated(forRemoval = true)
 public class ThreadUtil {
     public static final ReentrantLock MINT_MELT_LOCK = new ReentrantLock();
-    public static final ReentrantLock PROOF_STATE_LOCK = new ReentrantLock();
 }
