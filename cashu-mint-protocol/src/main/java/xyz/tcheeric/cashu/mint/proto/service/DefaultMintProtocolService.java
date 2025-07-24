36d379d97ef621b51b463d4c70f11f7a7a728494
Eric T <tchepane@hey.com>
2025-07-24 22:08:04 +0100

    Introduce MintProtocolService wrapper and inject into tasks

---
+++ cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/service/DefaultMintProtocolService.java
@@ -0,0 +1,37 @@
+package xyz.tcheeric.cashu.mint.proto.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.NonNull;
+import xyz.tcheeric.cashu.common.Mint;
+import xyz.tcheeric.cashu.common.PaymentMethod;
+import xyz.tcheeric.cashu.common.PrivateKey;
+import xyz.tcheeric.cashu.common.Proof;
+import xyz.tcheeric.cashu.common.util.CashuErrorException;
+import xyz.tcheeric.cashu.gateway.Gateway;
+import xyz.tcheeric.cashu.mint.proto.util.MintProtocolUtil;
+import xyz.tcheeric.cashu.vault.db.model.MintEntity;
+import xyz.tcheeric.cashu.vault.db.model.ProofEntity;
+
+@Service
+public class DefaultMintProtocolService implements MintProtocolService {
+    @Override
+    public Gateway createGateway(@NonNull PaymentMethod method) {
+        return MintProtocolUtil.createGateway(method);
+    }
+
+    @Override
+    public PrivateKey getPrivateKey(@NonNull String keySetId, @NonNull Integer amount, @NonNull Mint mint) throws CashuErrorException {
+        return MintProtocolUtil.getPrivateKey(keySetId, amount, mint);
+    }
+
+    @Override
+    public MintEntity toMintEntity(@NonNull Mint mint) {
+        return MintProtocolUtil.toMintEntity(mint);
+    }
+
+    @Override
+    public <T extends xyz.tcheeric.cashu.common.Secret> ProofEntity toProofEntity(@NonNull Proof<T> proof, @NonNull MintEntity mintEntity) {
+        return MintProtocolUtil.toProofEntity(proof, mintEntity);
+    }
+}
