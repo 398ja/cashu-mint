ecd08d381ac110771d4160b79632d13e0b7eca7f
erict875 <erict875@tuta.io>
2025-07-26 18:00:20 +0100

    refactor: remove commented-out melt test from MeltTest

--- cashu-mint-protocol/src/test/java/xyz/tcheeric/cashu/mint/proto/tasks/CheckStateTaskTest.java
+++ cashu-mint-protocol/src/test/java/xyz/tcheeric/cashu/mint/proto/tasks/CheckStateTaskTest.java
@@ -3,6 +3,7 @@ package xyz.tcheeric.cashu.mint.proto.tasks;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import xyz.tcheeric.cashu.common.Mint;
+import xyz.tcheeric.cashu.common.RandomStringSecret;
 import xyz.tcheeric.cashu.common.util.CashuErrorException;
 import xyz.tcheeric.cashu.entities.rest.PostCheckStateRequest;
 import xyz.tcheeric.cashu.entities.rest.PostCheckStateResponse;
@@ -27,7 +28,7 @@ public class CheckStateTaskTest {
     public void executeSuccess() throws CashuErrorException {
         UUID mintId = UUID.randomUUID();
         PostCheckStateRequest request = Mockito.mock(PostCheckStateRequest.class);
-        when(request.getHashToCurveSecrets()).thenReturn(List.of("secret"));
+        when(request.getHashToCurveSecrets()).thenReturn(List.of(RandomStringSecret.create()));
 
         MintProtocolService mintProtocolService = Mockito.mock(MintProtocolService.class);
         MintEntity mintEntity = Mockito.mock(MintEntity.class);
@@ -46,8 +47,8 @@ public class CheckStateTaskTest {
         verify(mintVaultService).load(mintEntity, false, true);
         verify(proofVaultService).retrieveProof("secret");
 
-        assertEquals(1, response.getResponseStates().size());
-        PostCheckStateResponse.ResponseState state = response.getResponseStates().get(0);
+        assertEquals(1, response.getStates().size());
+        PostCheckStateResponse.ResponseState state = response.getStates().get(0);
         assertEquals(NUT07.UNSPENT, state.getState());
         assertEquals("secret", state.getHashToCurveSecret());
         assertEquals("wit", state.getWitness());
