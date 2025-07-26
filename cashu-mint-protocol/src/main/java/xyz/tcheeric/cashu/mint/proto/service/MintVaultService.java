e2568048f13d926097aaf075e610fca5a6533210
Eric T <tchepane@hey.com>
2025-07-26 17:04:33 +0100

    Refactor NUT07 to use vault service

--- cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/service/MintVaultService.java
+++ cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/service/MintVaultService.java
@@ -5,4 +5,6 @@ import xyz.tcheeric.cashu.vault.db.model.MintEntity;
 
 public interface MintVaultService {
     MintEntity retrieveMint(String mintId) throws CashuErrorException;
+
+    void load(MintEntity mintEntity, boolean archive, boolean create) throws CashuErrorException;
 }
