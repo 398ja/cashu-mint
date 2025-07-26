10b91dccee7d77a2616178f607ce4c220e203f50
erict875 <erict875@tuta.io>
2025-07-26 10:23:17 +0100

    refactor: update MintLoadService to throw CashuErrorException

--- cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/service/DefaultMintLoadService.java
+++ cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/service/DefaultMintLoadService.java
@@ -2,6 +2,7 @@ package xyz.tcheeric.cashu.mint.proto.service;
 
 import org.springframework.stereotype.Service;
 import xyz.tcheeric.cashu.common.Mint;
+import xyz.tcheeric.cashu.common.util.CashuErrorException;
 import xyz.tcheeric.cashu.vault.api.db.impl.DBMintVault;
 
 import java.util.UUID;
@@ -9,7 +10,7 @@ import java.util.UUID;
 @Service
 public class DefaultMintLoadService implements MintLoadService {
     @Override
-    public Mint load(UUID mintId, boolean archive) {
+    public Mint load(UUID mintId, boolean archive) throws CashuErrorException {
         return DBMintVault.load(mintId, archive);
     }
 }
