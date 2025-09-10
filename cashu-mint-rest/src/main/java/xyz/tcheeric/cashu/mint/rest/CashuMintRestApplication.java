78492f91709abe174d926377fea2054a14bef3b6
erict875 <erict875@tuta.io>
2025-09-10 01:21:01 +0100

    fix(rest): rely on component scanning for MintInfo, drop redundant @EnableConfigurationProperties to avoid context conflicts in tests

--- cashu-mint-rest/src/main/java/xyz/tcheeric/cashu/mint/rest/CashuMintRestApplication.java
+++ cashu-mint-rest/src/main/java/xyz/tcheeric/cashu/mint/rest/CashuMintRestApplication.java
@@ -2,11 +2,8 @@ package xyz.tcheeric.cashu.mint.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import xyz.tcheeric.cashu.mint.proto.util.MintInfo;
 
 @SpringBootApplication(scanBasePackages = "xyz.tcheeric")
-@EnableConfigurationProperties(MintInfo.class)
 public class CashuMintRestApplication {
 
     public static void main(String[] args) {
