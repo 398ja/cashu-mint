2d4e8bdbb5218fc90de44fadc638e4671499edbd
erict875 <erict875@tuta.io>
2025-09-10 22:53:29 +0100

    fix(rest): narrow component scan to xyz.tcheeric.cashu to avoid autowiring external gateway beans (PhoenixdGateway) and placeholder failures

--- cashu-mint-rest/src/main/java/xyz/tcheeric/cashu/mint/rest/CashuMintRestApplication.java
+++ cashu-mint-rest/src/main/java/xyz/tcheeric/cashu/mint/rest/CashuMintRestApplication.java
@@ -3,7 +3,8 @@ package xyz.tcheeric.cashu.mint.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "xyz.tcheeric")
+// Limit component scanning to Cashu packages to avoid picking up external gateway beans
+@SpringBootApplication(scanBasePackages = "xyz.tcheeric.cashu")
 public class CashuMintRestApplication {
 
     public static void main(String[] args) {
