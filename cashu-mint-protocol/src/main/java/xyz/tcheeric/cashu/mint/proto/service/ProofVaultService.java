a64d20c9f026ecdcc435e91408f7356254bcb098
erict875 <erict875@tuta.io>
2025-09-30 23:36:02 +0100

    fix(proof-vault): add CashuErrorException to method signatures
    - Updated method signatures in ProofVaultService to throw CashuErrorException.
    - This change improves error handling for operations involving ProofEntity.

--- cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/service/ProofVaultService.java
+++ cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/service/ProofVaultService.java
@@ -1,11 +1,12 @@
 package xyz.tcheeric.cashu.mint.proto.service;
 
+import xyz.tcheeric.cashu.common.util.CashuErrorException;
 import xyz.tcheeric.cashu.vault.db.model.ProofEntity;
 
 public interface ProofVaultService {
-    void store(ProofEntity proofEntity);
-    void invalidate(ProofEntity proofEntity);
-    void archive(ProofEntity proofEntity);
-    void storePending(ProofEntity proofEntity);
-    ProofEntity retrieveProof(String secret);
+    void store(ProofEntity proofEntity) throws CashuErrorException;
+    void invalidate(ProofEntity proofEntity) throws CashuErrorException;
+    void archive(ProofEntity proofEntity) throws CashuErrorException;
+    void storePending(ProofEntity proofEntity) throws CashuErrorException;
+    ProofEntity retrieveProof(String secret) throws CashuErrorException;
 }
