e540f44bbce4539b05bc8d8b4a3c782b471b7f3b
tcheeric <erict875@tuta.io>
2025-12-22 23:14:20 +0000

    feat: implement DefaultDLEQProofGenerator for NUT-12 support
    - Added `DefaultDLEQProofGenerator` as the default implementation of `DLEQProofGenerator` for generating blind signature proofs.
    - Defined `DLEQProofGenerator` interface to standardize proof generation methods.

---
+++ cashu-mint-protocol/src/main/java/xyz/tcheeric/cashu/mint/proto/service/impl/DefaultDLEQProofGenerator.java
@@ -0,0 +1,25 @@
+package xyz.tcheeric.cashu.mint.proto.service.impl;
+
+import lombok.NonNull;
+import org.bouncycastle.math.ec.ECPoint;
+import xyz.tcheeric.cashu.common.DLEQProof;
+import xyz.tcheeric.cashu.crypto.DLEQUtils;
+import xyz.tcheeric.cashu.mint.proto.service.DLEQProofGenerator;
+
+import java.math.BigInteger;
+
+/**
+ * Default implementation for generating NUT-12 DLEQ proofs using {@link DLEQUtils}.
+ */
+public class DefaultDLEQProofGenerator implements DLEQProofGenerator {
+
+    @Override
+    public DLEQProof generateProof(
+            @NonNull BigInteger privateKey,
+            @NonNull ECPoint blindedMessage,
+            @NonNull ECPoint blindSignature
+    ) {
+        var proof = DLEQUtils.generateProof(privateKey, blindedMessage, blindSignature);
+        return DLEQProof.forBlindSignature(proof.e(), proof.s());
+    }
+}
